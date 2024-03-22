e92bf6f2f9a7db941f24b36fe336d5a727e1d3ba
ReDAndFel <pipecar366@gmail.com>
2024-03-22 14:30:20 -0500

    Se agrego funcionalidad para leer json, además de leer la matriz

--- AppPy/services/AlgorithmService.py
+++ AppPy/services/AlgorithmService.py
@@ -1,4 +1,4 @@
 from . import JsonService as js
 
-def add_value(json_file_path, value):
-        js.modify_property(json_file_path, "NaivOnArray", value)
+def add_time(json_file_path, algorithm_name, value):
+        js.modify_property(json_file_path, algorithm_name, value)
